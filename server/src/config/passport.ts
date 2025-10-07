75efe83e2da628b145756a9b05175c54536cc18c
Avinash <avinashnarwariya5@gmail.com>
2025-10-07 17:47:03 +0530

    callback url backend fix

--- server/src/config/passport.ts
+++ server/src/config/passport.ts
@@ -10,7 +10,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-      callbackURL: "http://localhost:8080/auth/google/callback",
+      callbackURL: "https://juris-ai.onrender.com/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
