814d20f966edbd037ee75c0dc4d39cf6841c2092
Avinash <avinashnarwariya5@gmail.com>
2025-10-07 16:40:25 +0530

    that same dynamic routing error

--- client/src/app/(private)/dashboard/contract/[id]/page.tsx
+++ client/src/app/(private)/dashboard/contract/[id]/page.tsx
@@ -1,13 +1,10 @@
-"use client";
-
 import ContractResults from "./_components/contract-results";
 
 interface IContractResultsProps {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 }
 
-export default function ContractPage({
-  params: { id },
-}: IContractResultsProps) {
+export default async function ContractPage({ params }: IContractResultsProps) {
+  const { id } = await params;
   return <ContractResults contractId={id} />;
 }
